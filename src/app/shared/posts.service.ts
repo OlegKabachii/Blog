0f6d30215f181ecd91e32b849d675c35ca8549f8
OlegK <kabachiyoleg@gmail.com>
2021-01-22 16:07:31 +0100

    show posts on dashboard

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -17,7 +17,20 @@ export class PostsService {
           ...post,
           id: response.name,
           date: new Date(post.date)
-        }
+        };
+      }));
+  }
+
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: { [key: string]: any }) => {
+        return Object
+          .keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }));
       }));
   }
 }
