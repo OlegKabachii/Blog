456d2ff92f3f43d62c596e5d9c0c30c3e70c46ab
OlegK <kabachiyoleg@gmail.com>
2021-01-22 13:58:48 +0100

    create post

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment:Environment = {
   production: true,
-  apiKey: 'AIzaSyAA1jt0JxEeYPEkhYS7e-4QT87JdpAXrcU'
+  apiKey: 'AIzaSyAA1jt0JxEeYPEkhYS7e-4QT87JdpAXrcU',
+  fbDbUrl: 'https://myblog-a0144-default-rtdb.firebaseio.com'
 };
