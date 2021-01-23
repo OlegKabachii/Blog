ace5708e8858298244a360e3229bfdb02aca2abe
OlegK <kabachiyoleg@gmail.com>
2021-01-23 14:55:49 +0100

    create plugin alert

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostsService} from '../../shared/posts.service';
 import {Post} from '../../shared/interfaces';
 import {Subscription} from 'rxjs';
+import {AlertService} from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -15,7 +16,9 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   dSub: Subscription;
   searchStr = '';
 
-  constructor(private postsService: PostsService) {
+  constructor(
+    private postsService: PostsService,
+    private alertService: AlertService) {
   }
 
   ngOnInit(): void {
@@ -36,6 +39,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   remove(id: string) {
   this.postsService.remove(id).subscribe(()=>{
     this.posts = this.posts.filter(post => post.id !== id)
+    this.alertService.warning('Post deleted!')
   })
   }
 }
